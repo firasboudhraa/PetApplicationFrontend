8bcbc1b0178651038fb7814bb90e180f6aafeb53
Aymen-thabet <Thabet.aymen1@gmail.com>
2025-04-24 01:16:52 +0100

    added fb api

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -7,18 +7,69 @@ import { Post } from 'src/app/models/Post'; // Adjust the import path as necessa
   providedIn: 'root'
 })
 export class PostsService {
+  
+private fbAccessToken = '${environment.FACEBOOK_ACCESS_TOKEN}'; 
+private fbApiUrl = `https://graph.facebook.com/me/feed`;
+
 
   private apiUrl = 'http://localhost:8082/posts'; // Update with your posts service URL
 
   constructor(private http: HttpClient) { }
+//facebook 
+publishToFacebook(message: string): Observable<any> {
+  const body = {
+    message: message,
+    access_token: this.fbAccessToken
+  };
+  return this.http.post(this.fbApiUrl, body);
+}
 
   // Get all posts
   getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(this.apiUrl);
   }
-  addPost(postData: any, userId: number): Observable<Post> {
-    return this.http.post<Post>(`${this.apiUrl}/${userId}`, postData);
+  // addPost(postData: any, userId: number): Observable<Post> {
+  //   return this.http.post<Post>(`${this.apiUrl}/${userId}`, postData);
+  // }
+  uploadPhotoToFacebook(imageUrl: string, caption: string): Observable<any> {
+    const body = {
+      url: imageUrl, // This must be a public URL
+      caption: caption,
+      access_token: this.fbAccessToken
+    };
+    return this.http.post(this.apiUrl, body);
   }
+  
+  addPost(postData: any, userId: number): Observable<Post> {
+    return new Observable<Post>((observer) => {
+      this.http.post<Post>(`${this.apiUrl}/${userId}`, postData).subscribe({
+        next: (createdPost) => {
+          const message = `📝 ${postData.title}\n\n${postData.content}`;
+          const imageUrl = postData.imageUrl; // Must be a PUBLIC URL
+  
+          if (imageUrl) {
+            this.uploadPhotoToFacebook(imageUrl, message).subscribe({
+              next: () => observer.next(createdPost),
+              error: (fbErr) => {
+                console.error('Facebook photo upload failed', fbErr);
+                observer.next(createdPost);
+              }
+            });
+          } else {
+            this.publishToFacebook(message).subscribe({
+              next: () => observer.next(createdPost),
+              error: (fbErr) => {
+                console.error('Facebook text post failed', fbErr);
+                observer.next(createdPost);
+              }
+            });
+          }
+        },
+        error: (err) => observer.error(err)
+      });
+    });
+  }
+  
   
   
   deletePost(postId: number): Observable<void> {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: false,
     apiBaseUrl: 'http://127.0.0.1:5000',
-    apiGemini: "AIzaSyDpzYkOOebvdY2pLKZwUm_uSkWR0OPqoZg"
+    apiGemini: "AIzaSyDpzYkOOebvdY2pLKZwUm_uSkWR0OPqoZg",
+    FACEBOOK_ACCESS_TOKEN: "EAAQqWxdDokEBOZBgZCSIW0NaGrOZCdHe1aoPyM8fzooVgP6o6yxUQQQIQkAN9Q2ENZCmYSGevnljZAAPpJEZBlGjNJ7ggqAIczSYfcZBGiV8bm2ZCg1uScxc7CJtaKr8wtZBwfemFbcyZAphotkjGli8YFEx2dax9p869x0nIZCr4ZA8gRbcBhV4OpuSUjcQth50"
   };
 
 
