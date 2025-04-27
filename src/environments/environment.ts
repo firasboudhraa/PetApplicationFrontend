4afdbc73c1e7b3eac6ad1bb0ec6f149a735ee2e3
rayen <rayen.trabelsi@esprit.tn>
2025-04-27 17:24:24 +0200

    edits

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   googleMapsApiKey: 'AIzaSyAnJDpXIRlCd7a8zSKpXn_iL9lzXBNHPuc',
-  AiapiKey: 'Key',
-  paypalClientId: 'ClientID',
+  AiapiKey: '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',
+  paypalClientId: 'AcRNmlTOelCjXZB_LhOBQdj4nuEprFipRanFAPL7oe4TQuxaQHau7Zgh_wAB76TpkqbuqNL1OSpqxIzp',
 }
